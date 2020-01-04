8e4d75bcb2932a91fa751b35bb09b02effdbda1d
Pierre-Guillaume Laurin <pierreguillaumelaurin@gmail.com>
2020-01-04 14:33:10 -0500

    Refactor solution.py.

--- 4/solution.py
+++ 4/solution.py
@@ -7,14 +7,14 @@ class Solution():
     def __init__(self, password_range: str):
         self.lower_limit = int(password_range[:6])
         self.upper_limit = int(password_range[7:])
-        self.password = [str(n) for n in range(self.lower_limit, self.upper_limit)]
+        self.passwords = [str(n) for n in range(self.lower_limit, self.upper_limit)]
 
     def one(self) -> int:
         # going from left to right, the digits never decrease
-        self.password = [n for n in self.password if n == "".join(sorted(n))]    
+        self.passwords = [n for n in self.passwords if n == "".join(sorted(n))]    
         # two adjacent digits are the same
-        self.password = [n for n in self.password if any(map(operator.eq, n, n[1:]))]
-        return len(self.password)
+        self.passwords = [n for n in self.passwords if any(map(operator.eq, n, n[1:]))]
+        return len(self.passwords)
 
     def two(self) -> int:
-        return sum((1 for n in self.password if 2 in collections.Counter(n).values()))
+        return sum((1 for n in self.passwords if 2 in collections.Counter(n).values()))
